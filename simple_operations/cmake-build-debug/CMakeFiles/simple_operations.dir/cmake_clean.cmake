86fc5d16abced4181e84d94b8789977c56a23f4b
diogolopes18-cyber <diogolopes18@ieee.org>
2021-10-02 10:38:02 +0100

    (fix): change project name on CMakeLists

---
+++ simple_operations/cmake-build-debug/CMakeFiles/simple_operations.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_operations.dir/main.cpp.o"
+  "simple_operations"
+  "simple_operations.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_operations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
