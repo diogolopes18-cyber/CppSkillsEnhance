fd8c96ba2e1af3eb8fa00886053b09cfbc24f948
diogolopes18-cyber <diogolopes18@ieee.org>
2021-10-02 20:40:48 +0100

    (feature): define class method

--- simple_operations/credit_card_verifier/CreditCardInfo.cpp
+++ simple_operations/credit_card_verifier/CreditCardInfo.cpp
@@ -4,10 +4,34 @@
 
 void specifyCreditCardInfo()
 {
-    CreditCard card;
+    //Without constructor
+    /*CreditCard card;
     card.creditCardNumber = 2102120;
     card.cvcNumber = 222;
-    card.bankNam = "CGD";
+    card.bankNam = "CGD";*/
 
-    std::cout << card.bankNam;
+    //Create new object for class CreditCardInfo
+    CreditCard card(20221232, 222, "CGD");
+    if(card.creditCardIsValid(card.creditCardNumber) == true)
+        std::cout << "Valid card!" << std::endl;
+    else
+        std::cout << "Invalid card! Please insert a new card";
+
+}
+
+bool CreditCard::creditCardIsValid(int creditCardNumber) {
+
+    int cnt = 0;
+    while(creditCardNumber != 0)
+    {
+        creditCardNumber /= 10;
+        cnt++;
+    }
+
+    if (cnt != 16)
+        perror("Should contain 16 digits!");
+    else
+        return true;
+
+    return false;
 }
