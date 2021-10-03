19117ec3351bb6daba33840854277e2e812a621e
diogolopes18-cyber <diogolopes18@ieee.org>
2021-10-03 19:03:08 +0100

    (fix): switch from map to array

--- simple_operations/credit_card_verifier/CreditCardInfo.cpp
+++ simple_operations/credit_card_verifier/CreditCardInfo.cpp
@@ -5,19 +5,25 @@
 
 void specifyCreditCardInfo()
 {
-    //Without constructor
-    /*CreditCard card;
-    card.creditCardNumber = 2102120;
-    card.cvcNumber = 222;
-    card.bankNam = "CGD";*/
+    char cardDetermine;
 
     //Create new object for class CreditCardInfo
     CreditCard card(20221232, 222, "CGD");
-    if(card.creditCardIsValid(card.creditCardNumber) == true)
+    if(card.creditCardIsValid(card.creditCardNumber) == true) {
         std::cout << "Valid card!" << std::endl;
-    else
-        std::cout << "Invalid card! Please insert a new card";
 
+        //Choose to proceed with card determination
+        std::cout << "Proceed with card determination?\n";
+        std::cin >> cardDetermine;
+
+        if(cardDetermine == 'y')
+            card.typeOfCard(card.creditCardNumber);
+        else
+            std::cout << "Exiting";
+    }
+
+    else
+        std::cout << "Invalid card! Please insert a new card\n";
 }
 
 bool CreditCard::creditCardIsValid(int creditCardNumber) {
@@ -37,23 +43,47 @@ bool CreditCard::creditCardIsValid(int creditCardNumber) {
     return false;
 }
 
-std::string CreditCard::typeOfCard(int creditCardNumber) {
+void CreditCard::typeOfCard(int creditCardNumber) {
 
     //Defines a dictionary with the available types of IIN numbers for credit cards
-    std::map<char, int> availableTypes = {
-            {'American Express', 3},
-            {'Visa', 4},
-            {'MasterCard', 5},
-            {'Diners Club', 6}
-    };
+    /*std::map<char, int> availableTypes = {
+            {'A', 3},
+            {'V', 4},
+            {'M', 5},
+            {'D', 6}
+    };*/
+
+    int availableTypes[] = {3, 4, 5, 6};
+    int firstDigit = (creditCardNumber % 10) / 10;
 
     //STILL NEED FIX
 
     //Loop through map to find a match between the first number
     // in the credit card and the values of the keys in the map
-    for (auto it=availableTypes.begin(); it!=availableTypes.end(); ++it)
+    for(int cardNum = 0; cardNum < 3; cardNum++)
     {
-        if(availableTypes.value_comp(it) == creditCardNumber)
-            std::cout << availableTypes.find(creditCardNumber);
+        if( firstDigit == availableTypes[cardNum])
+        {
+            switch (firstDigit) {
+                case 3:
+                    std::cout << "American Express Card" << std::endl;
+                    break;
+
+                case 4:
+                    std::cout << "Visa Card" << std::endl;
+                    break;
+
+                case 5:
+                    std::cout << "MasterCard" << std::endl;
+                    break;
+
+                case 6:
+                    std::cout << "Diners Club Card" << std::endl;
+                    break;
+            }
+        }
+
+        else
+            exit(-1);
     }
 }
