6c48af6f2781927bbacd03db35d7b15f12a32575
diogolopes18-cyber <diogolopes18@ieee.org>
2021-10-10 18:34:39 +0100

    (feature): new directory for playing with dynamic arrays

---
+++ simple_operations/dynamic_arrays/dynamicArrays.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+//Simple program to illustrate the usage of dynamic arrays
+
+int specifySize()
+{
+    int size;
+    std::cout << "Please insert the size of your array: \n";
+    std::cin >> size;
+
+    return size;
+}
+
+void createDynamicArray()
+{
+    int arraySize = specifySize();
+
+    //Create a dynamic array
+    //In order to create a new dynamic array, we use the keyword new
+    //We declare a pointer to the new dynamic array
+
+    int* myArray = new int [arraySize];
+
+    //Fill in the array
+    for(int i = 0; i < arraySize; i++)
+    {
+        std::cin >> myArray[i];
+    }
+
+    std::string choice;
+    std::cout << "Do you still wish to add content to the array?\n";
+    std::cin >> choice;
+
+    if(choice == "y" || choice == "yes")
+    {
+        delete[]myArray;
+        myArray = NULL;
+        specifySize();
+    }
+    else {
+        delete[]myArray;
+        myArray = NULL;
+    }
+}
---
+++ simple_operations/dynamic_arrays/dynamicArrays.h
@@ -0,0 +1,11 @@
+//
+// Created by diogo on 10/10/2021.
+//
+
+#ifndef SIMPLE_OPERATIONS_DYNAMICARRAYS_H
+#define SIMPLE_OPERATIONS_DYNAMICARRAYS_H
+
+int specifySize();
+void createDynamicArray();
+
+#endif //SIMPLE_OPERATIONS_DYNAMICARRAYS_H
